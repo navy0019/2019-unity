f37ee986e89126409d5fa90cc026c76257fba229
navy0019 <36374046+navy0019@users.noreply.github.com>
2020-08-14 18:18:23 +0800

    2019

---
+++ code/Astar.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astar
+{
+
+    List<Node> close;
+    List<Node> open;
+    List<Vector3> path;
+
+    public void InitList()
+    {
+        close = new List<Node>();
+        open = new List<Node>();
+
+    }
+    //startAStar都會創造一個新的hashtable
+    public List<Vector3> startAStar(Node start, Node end)
+    {
+
+        path = new List<Vector3>();
+        close.Clear();
+        open.Clear();
+
+        Node startN = start;
+        Node endN = end;
+
+        open.Add(startN);
+        while (open.Count != 0)
+        {
+            Node now = open[0];
+            for (int i = 0; i < open.Count; i++)
+            {
+                if (open[i].f < now.f)
+                {
+                    now = open[i];
+                }
+            }
+
+            open.Remove(now);
+            close.Add(now);
+            if (now == endN)
+            {
+                buildPath(startN, endN, path);
+                break;
+            }
+            foreach (Node n in now.neighbor)
+            {
+                if (close.Contains(n))
+                {
+                    continue;
+                }
+                float newCost = now.g + getLength(now, n);
+
+                if (newCost < n.g || !open.Contains(n))
+                {
+                    n.g = newCost;
+                    n.h = getLength(n, endN);
+                    n.parent = now;
+
+                    if (!open.Contains(n))
+                    {
+                        open.Add(n);
+                    }
+                }
+            }
+        }
+        return path;
+    }
+    private float getLength(Node start, Node target)
+    {
+        Vector3 v = start.pos - target.pos;
+        float dist = v.magnitude;
+        return dist;
+    }
+
+    private void buildPath(Node start, Node end, List<Vector3> path)
+    {
+        Node now = end;
+        while (now != start)
+        {
+            path.Add(now.pos);
+            now = now.parent;
+            if (now == start)
+            {
+                path.Add(now.pos);
+            }
+        }
+        path.Reverse();
+
+    }
+}
---
+++ code/BagUIView.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BagUIView : MonoBehaviour
+{
+
+    private AllObject itemInfo;
+    private ItemData[] bagInfo;
+
+    public GameObject playerData;
+    public Text moneyText;
+    public Text HPText;
+    public Text MPText;
+    public Text AttackText;
+    public Text DefText;
+
+    private void Start()
+    {
+
+        itemInfo = AllObject.instance;
+
+        bagInfo = itemInfo.bagInfo;
+
+        itemInfo.changeBag += updateBagView;
+
+        updateBagView();
+
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            CheckBagTransform();
+        }
+    }
+    //check bag item's transform is correct
+    public void CheckBagTransform()
+    {
+        //Debug.Log("check");
+        if (Input.GetMouseButton(0))
+        {
+            //Debug.Log("still down");
+        }
+        else
+        {
+            for (int i = 0; i < bagInfo.Length; i++)
+            {
+                Transform gridChild = AllObject.instance.gridParent.transform.GetChild(i);
+                if (bagInfo[i] != null && gridChild.transform.childCount == 0)
+                {
+                    AllObject.instance.gridParent.transform.parent.GetChild(10).transform.parent = gridChild;
+                    gridChild.GetChild(0).transform.localPosition = Vector3.zero;
+                    gridChild.GetChild(0).GetChild(0).GetComponent<Image>().raycastTarget = true;
+                }
+            }
+        }
+
+    }
+    public void updateBagView()
+    {
+        //Debug.Log("updataItem");
+        for (int i = 0; i < bagInfo.Length; i++)
+        {
+            Transform gridChild = AllObject.instance.gridParent.transform.GetChild(i);
+            ItemData itemNow = bagInfo[i];
+            if (itemNow != null && gridChild.transform.childCount != 0)
+            {
+                gridChild.transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = bagInfo[i].img;
+
+                if (itemNow.number > itemNow.MaxNumber)
+                {
+                    int tempNum = itemNow.number;
+
+
+                    MakeBagItemNumber(tempNum ,itemNow);
+                }
+                else
+                {
+                    gridChild.transform.GetChild(0).GetChild(1).GetComponent<Text>().text = itemNow.number.ToString();
+                    //Debug.Log("small");
+                }
+            }
+        }
+        moneyText.text = AllObject.instance.itemDic["money"].number.ToString();
+        HPText.text= playerData.GetComponent<CharacterFakeData>().Hp.ToString();
+        MPText.text = playerData.GetComponent<CharacterFakeData>().Mp.ToString();
+        AttackText.text = playerData.GetComponent<CharacterFakeData>().Attack.ToString();
+        DefText.text = playerData.GetComponent<CharacterFakeData>().Defense.ToString();
+    }
+
+    public void MakeBagItemNumber(int tempNum , ItemData itemNow)
+    {
+        for (int k = 0; k < bagInfo.Length && tempNum != 0; k++)
+        {
+            Transform gridChild;
+
+            if (AllObject.instance.gridParent.transform.childCount != 0)
+            {
+                 gridChild = AllObject.instance.gridParent.transform.GetChild(k);
+            }
+            else
+            {
+                continue;
+            }
+
+            if (bagInfo[k] != null && bagInfo[k].name == itemNow.name)
+            {
+                if (gridChild.transform.childCount == 0)
+                {
+                    continue;
+                }
+                if (tempNum > itemNow.MaxNumber)
+                {
+                    gridChild.transform.GetChild(0).GetChild(1).GetComponent<Text>().text = itemNow.MaxNumber.ToString();
+                    tempNum -= itemNow.MaxNumber;
+                    //Debug.Log("max  "+ tempNum);
+                }
+                else
+                {
+                    gridChild.transform.GetChild(0).GetChild(1).GetComponent<Text>().text = tempNum.ToString();
+                    tempNum = 0;
+                    //Debug.Log("%%%%%%  ");
+                }
+            }
+
+        }
+    }
+
+}
